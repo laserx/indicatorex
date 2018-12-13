a67d95d3c55c4db0a5098018da4a93ad79c72fd4
laserx <laserxie@gmail.com>
2018-12-13 23:25:02 +0800

    modify ema logic

--- lib/ema.ex
+++ lib/ema.ex
@@ -2,22 +2,22 @@ defmodule Indicatorex.EMA do
   @type t :: %Indicatorex.EMA{span: integer, v: [float]}
   defstruct span: 0, v: []
 
-  @spec calc([number()], number()) :: {:error, :empty} | {:ok, Indicatorex.EMA.t()}
-  def calc(data, span), do: run(data, span)
+  @spec calc([number()], number()) :: {:error, String.t()} | {:ok, Indicatorex.EMA.t()}
+  def calc(data, span) when span >= 1, do: run(data, span)
+  def calc(_, span), do: {:error, "span value: #{span}. span must above 1"}
 
-  @spec calc(any(), any(), any()) :: float() | {:error, String.t()}
-  def calc(close, pre, span) when span >= 1, do: (2 * close + (span - 1) * pre) / (span + 1)
-  def calc(_, _, span), do: {:error, "span value: #{span}. span must above 1"}
+  @spec calc(number(), number(), number()) :: float()
+  def calc(close, pre, span),
+    do: (2 * close + (span - 1) * pre) / (span + 1)
 
   defp run(data, span, resp \\ [])
-  defp run([], _span, []), do: {:error, :empty}
+  defp run([], _span, []), do: {:error, "empty"}
   defp run([], span, resp), do: {:ok, %Indicatorex.EMA{span: span, v: resp}}
   defp run([h | t], span, []), do: run(t, span, [h / 1])
 
   defp run([h | t], span, resp) do
-    [pre_average] = Enum.take(resp, -1)
-    new_weight = 2 / (span + 1)
-    new_average = h * new_weight + pre_average * (1 - new_weight)
-    run(t, span, resp ++ [new_average])
+    [pre] = Enum.take(resp, -1)
+    new = calc(h, pre, span)
+    run(t, span, resp ++ [new])
   end
 end
--- test/indicatorex_test.exs
+++ test/indicatorex_test.exs
@@ -1,8 +1,4 @@
 defmodule IndicatorexTest do
   use ExUnit.Case
   doctest Indicatorex
-
-  test "greets the world" do
-    assert Indicatorex.hello() == :world
-  end
 end
