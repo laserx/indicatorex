ec23322cb8abcc8dabcfff53f8d72b76b3f20003
laserx <laserxie@gmail.com>
2018-12-14 14:45:28 +0800

    add type of MACD.Sericalize

--- lib/macd/sericalize.ex
+++ lib/macd/sericalize.ex
@@ -1,3 +1,10 @@
 defmodule Indicatorex.MACD.Sericalize do
+  @type t :: %Indicatorex.MACD.Sericalize{
+          ema_f: number(),
+          ema_s: number(),
+          dea: number(),
+          dif: number(),
+          macd: number()
+        }
   defstruct ema_f: 0, ema_s: 0, dea: 0, dif: 0, macd: 0
 end
